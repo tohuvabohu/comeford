8c9ea0422958cbd7b6a7e2c1d83bd534987f343b
Sean Higgins <shiggins@Seans-MacBook.local>
2019-10-24 15:11:42 -0700

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Welcome!</h1>
+    <p>Comeford College is a Kuyperian, Christian college in Marysville, WA.</p>
+    <p>Classes start Fall 2020, <em>Deo volente</em>!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
