bf709c40166af794814167f340fedabdc4ebf5cb
Sean Higgins <shiggins@Seans-MacBook.local>
2019-10-28 11:33:37 -0700

    updated Board page

--- src/pages/board.js
+++ src/pages/board.js
@@ -9,13 +9,13 @@ const BoardPage = () => (
     <SEO title="Board" />
     <h1>Board</h1>
     <h2>President</h2>
-    <p>Jim Martin</p>
+    <p>Jim Martin <a href="mailto:jimngail4@gmail.com">Email</a></p>
     <h2>Members</h2>
-    <p>Mark Callender</p>
-    <p>John Giddings</p>
-    <p>Sean Higgins</p>
-    <p>Philip Kulishov</p>
-    <p>Jonathan Sarr</p>
+    <p>Mark Callender <a href="mailto:mark.callender@quantivate.com">Email</a></p>
+    <p>John Giddings <a href="mailto:jwgidd@gmail.com">Email</a></p>
+    <p>Sean Higgins <a href="mailto:seankhiggins@gmail.com">Email</a></p>
+    <p>Philip Kulishov <a href="mailto:kulishov.philip@gmail.com">Email</a></p>
+    <p>Jonathan Sarr <a href="mailto:jonathansarr@gmail.com">Email</a></p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
