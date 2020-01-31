24d86195756b8d41b632b1febcc7effc16cd0df3
Sean Higgins <shiggins@Seans-MacBook.local>
2020-01-30 22:42:19 -0800

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <p>Comeford College is a new Christian college with Kuyperian interests in Marysville, WA.</p>
     <p>Classes start Fall 2020, <em>Deo volente</em>!</p><br/>
-    <h2>Inagural Information Night!</h2>
+    <h2>Inaugural Information Night!</h2>
     <p>Thursday, March 19<br/>
     7:00 PM<br/>
     <a href="http://maps.google.com/maps?q=9015+44th+Dr.+NE++Marysville+WA+98270">9015 44th Dr. NE</a><br/><br/>
