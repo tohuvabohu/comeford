d0e730221e70bd2ba85b3259952e1537f57b8dfe
Sean Higgins <shiggins@Seans-MacBook.local>
2020-05-11 09:36:11 -0700

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <p>Comeford College is a new Christian college with Kuyperian interests in Marysville, WA.</p>
     <p>Classes start Fall 2020, <em>Deo volente</em>!</p><br/>
     <h2>Inaugural Information Night</h2>
-    <p><strong>Take Two</strong> May 19<br/>
+    <p><strong>Take Two! - May 19</strong><br/>
     7:00 PM<br/><br/>
     Let us know you're interested (or what questions you have already) on our <a href="https://www.facebook.com/events/236429261027529/">Facebook Event Page</a>.</p>
     <hr></hr>
