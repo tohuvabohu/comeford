ae00b05e2c1455e37f394049fdc158f8c04fcebb
Sean Higgins <shiggins@Seans-MacBook.local>
2020-05-11 09:44:08 -0700

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <p>Classes start Fall 2020, <em>Deo volente</em>!</p><br/>
     <h2>Inaugural Information Night</h2>
     <p><strong>Take Two! - May 19</strong><br/>
+    Via Zoom<br>
     7:00 PM<br/><br/>
     Let us know you're interested (or what questions you have already) on our <a href="https://www.facebook.com/events/236429261027529/">Facebook Event Page</a>.</p>
     <hr></hr>
@@ -20,5 +21,3 @@ const IndexPage = () => (
 )
 
 export default IndexPage
-
-
